a9e0e337340ddf92f747ef226f16811ae27f15b1
stevegaughan <stephen.p.gaughan@dartmouth.edu>
2025-04-22 09:17:37 -0400

    Update point-in-polygon-qgis.py

--- code/point-in-polygon-qgis.py
+++ code/point-in-polygon-qgis.py
@@ -1,1 +1,11 @@
 # this file: point-in-polygon-qgis.py
+
+# running this tool in the QGIS interface: 
+
+
+# python code
+from qgis.core import *
+import processing
+
+layer1 = processing.getObject('bear-sightings')
+layer2 = processing.getObject('10m_us_parks')
