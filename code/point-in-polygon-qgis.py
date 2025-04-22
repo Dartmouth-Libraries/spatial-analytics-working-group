133baa3114f7ce50b5121c55e0cf3e1f03ffd295
stevegaughan <stephen.p.gaughan@dartmouth.edu>
2025-04-22 09:20:23 -0400

    Update point-in-polygon-qgis.py

--- code/point-in-polygon-qgis.py
+++ code/point-in-polygon-qgis.py
@@ -7,5 +7,9 @@
 from qgis.core import *
 import processing
 
-layer1 = processing.getObject('bear-sightings')
-layer2 = processing.getObject('10m_us_parks')
+layer1 = processing.getObject('bearsightings')
+layer2 = processing.getObject('nationalparks.zip') # 10m US parks layer 
+
+processing.run("native:intersection", {'INPUT':'delimitedtext://file:///Downloads/bearsightings.csv?type=csv&maxFields=10000&detectTypes=yes&xField=longitude&yField=latitude&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=no','OVERLAY':'/vsizip//Downloads/nationalparks.zip/10m_us_parks_area.shp|layername=10m_us_parks_area','INPUT_FIELDS':[],'OVERLAY_FIELDS':[],'OVERLAY_FIELDS_PREFIX':'','OUTPUT':'TEMPORARY_OUTPUT','GRID_SIZE':None})
+
+
